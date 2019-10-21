43f83591e75ce58145dab468cc6c66b88ec50b96
caffeine <eniehack@outlook.jp>
2019-10-21 20:30:52 +0900

    fix design

--- pages/access.vue
+++ pages/access.vue
@@ -2,31 +2,32 @@
 div
   Hero(title="アクセス")
   div.container
-    div.columns.is-light.has-text-success.has-text-centered
-      div.column
-        article(class="title is-child notification is-light has-text-success has-text-centered")
-           div.content
-            b-icon(icon="car" size="is-large")
-            p.is-size-3-mobile 車
-            p.is-size-5-mobile.is-size-5-desktop.has-text-weight-light 一般の方は群馬高専 大駐車場に停めことができます。前橋方面の方はぐんまマラソンの交通規制もご確認ください。
-      div.column
-        article(class="title is-child notification is-light has-text-success has-text-centered")
     div.columns.is-multiline
       div.column.is-full
         Timer
+      div.column.is-half-desktop
+        article.notification.is-light.has-text-success
           div.content
-            b-icon(icon="train" size="is-large")
-            p.is-size-3-mobile 電車
+            p.is-size-3-mobile.is-size-3-desktop.has-text-weight-semibold
+              b-icon(icon="car" size="is-large")
+              span 車
+            p.is-size-5-mobile.is-size-5-desktop.has-text-weight-light 一般の方は群馬高専 大駐車場に停めことができます。前橋方面の方はぐんまマラソンの交通規制もご確認ください。
+      div.column.is-half-desktop
+        article.notification.is-light.has-text-success
+            p.is-size-3-mobile.is-size-3-desktop.has-text-weight-semibold
+              b-icon(icon="train" size="is-large")
+              span 電車
             p.is-size-5-mobile.is-size-5-desktop.has-text-weight-light JR上越線/両毛線/吾妻線 新前橋駅下車後シャトルバスでN分
-    div(class="tile is-parent is-vertical")
-        article(class="title is-child notification is-light has-text-success has-text-centered")
+      div.column
+        article.is-child.notification.is-light.has-text-success
           div.content
-            b-icon(icon="bus-clock" size="is-large")
-            p.is-size-3-mobile シャトルバス
+            p.is-size-3-mobile.is-size-3-desktop.has-text-weight-semibold
+              b-icon(icon="bus-clock" size="is-large")
+              span シャトルバス
             p.is-size-5-mobile.is-size-5-desktop.has-text-weight-normal 当日2019/11/02、2019/11/03は新前橋駅東口と群馬高専を結ぶシャトルバスが運行されます。
             div.columns.is-multiline.is-half-desktop.is-half-tablet
               div.column
-                p.is-size-3-mobile 新前橋駅東口 発 時刻表
+                p.is-size-3-mobile.has-text-weight-medium 新前橋駅東口 発 時刻表
                 table.table.is-bordered
                   thead
                     tr
@@ -40,7 +41,7 @@ div
                           span(v-if="minute === 0") 00
                           span(v-else) {{ minute }}
               div.column
-                p.is-size-3-mobile 群馬高専正門 発 時刻表
+                p.is-size-3-mobile.has-text-weight-medium 群馬高専正門 発 時刻表
                 table.table.is-bordered
                   thead
                     tr
@@ -58,10 +59,12 @@ div
 <script>
 import Hero from "@/components/Hero.vue";
 import Meta from "@/assets/mixins/meta.js";
+import Timer from "@/components/Timer.Vue";
 
 export default {
   components: {
-    Hero
+    Hero,
+    Timer
   },
   mixins: [
     Meta
