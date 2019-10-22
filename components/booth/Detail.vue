0884e8e3c94fe0c490cd46465151e1a904e98ae6
caffeine <eniehack@outlook.jp>
2019-10-23 02:14:30 +0900

    fix design

--- components/booth/Detail.vue
+++ components/booth/Detail.vue
@@ -7,11 +7,13 @@
             img(:src="'/22nd/images/' + circle + '.png'" :alt="circle +'のサークルカット'")
         div.column.has-text-left
           h1.title {{ item.title }}
-          p.has-text-weight-light
-            b-icon(icon="account" custom-size="mdi-24px")
-            span {{ circle }}
-            b-icon(icon="map-marker" custom-size="mdi-24px")
-            span {{ item.place }}
+          div.data
+            p.has-text-weight-light.grid
+              b-icon(icon="account" custom-size="mdi-24px")
+              span {{ circle }}
+            p.has-text-weight-light.grid
+                b-icon(icon="map-marker" custom-size="mdi-24px")
+                span {{ item.place }}
         a.button.is-warning
           b-icon(icon="star" custom-size="mdi-24px")
           span ブックマークに追加
@@ -21,10 +23,11 @@
       figure.image.is-128x128
         img(:src="'/22nd/images/' + circle + '.png'" :alt="circle +'のサークルカット'")
       div.content
-        h2.title {{ item.title }}
-        p.has-text-weight-light
+        h1.title {{ item.title }}
+        p.has-text-weight-light.grid
           b-icon(icon="account" custom-size="mdi-24px")
           span {{ circle }}
+        p.has-text-weight-light.grid
           b-icon(icon="map-marker" custom-size="mdi-24px")
           span {{ item.place }}
         a.button.is-warning
@@ -33,6 +36,17 @@
       div.content
         p {{ item.description }}
 </template>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: 27px 1fr;
+}
+.data {
+	display: grid;
+	grid-gap: 5px;
+}
+</style>
 
 <script>
 import moment from "moment";
