88531989afc733f5e0a94d370534937f9ddc8c49
caffeine <eniehack@outlook.jp>
2019-10-31 22:40:36 +0900

    ブックマークとジャンル別一覧をリンクとして掲載

--- components/Header.vue
+++ components/Header.vue
@@ -9,6 +9,8 @@
         b-navbar-item(tag="nuxt-link" :to="{ path: '/stages/list/' }") ステージ
         b-navbar-dropdown(label="企画" hoverable)
           b-navbar-item(tag="nuxt-link" :to="{ path: '/booths/list/' }") 企画一覧
+          b-navbar-item(tag="nuxt-link" :to="{ path: '/booths/genre/' }") ジャンル別企画一覧
+        b-navbar-item(tag="nuxt-link" :to="{ path: '/bookmark/' }") ブックマーク
       template(slot="end")
         a(class="navbar-item" href="https://twitter.com/kokasai_GNCT")
           b-icon(icon="twitter" size="is-medium")
