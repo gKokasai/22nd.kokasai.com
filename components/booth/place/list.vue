db145fbd824a2809b9afc8bb90f6fc076d051ce3
caffeine <eniehack@outlook.jp>
2019-10-31 22:38:21 +0900

    ブースをジャンル毎に検索できるようにした

---
+++ components/booth/place/list.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  div
+    Hero(:title="'ジャンル '+ place +'の企画一覧'")
+    div.container
+      figure.image
+        img(src="/22nd/images/tentsmap.svg" alt="Map" style="height: 600px;width: 480px;")
+      article.media(v-for="item in items")
+        figure.media-left
+            div.image.is-128x128
+              img(:src="'/22nd/images/' + item.circle + '.png'" :alt="item.circle +'のサークルカット'")
+        div.media-content
+          div.content
+            p.is-size-5-mobile.is-size-4-desktop.has-text-weight-bold
+              nuxt-link(:to="'/booths/' + item.circle") {{ item.title }}
+            p.has-text-weight-light
+              b-icon(icon="account" size="is-small")
+              span {{ item.circle }}
+            p.has-text-weight-light
+              b-icon(icon="map-marker" size="is-small")
+              span {{ item.place }}
+        div.media-right
+          b-icon(icon="star-outline" size="is-large")
+</template>
+
+<script>
+import Hero from "@/components/Hero.vue"
+
+export default {
+  props: ["items", "place"],
+  components: {
+    Hero
+  },
+  created() {
+    this.samePlaceBooths = this.items.filter(value => value["placeBlock"] !== this.place)
+  },
+  data() {
+    return {
+      samePlaceBooths: []
+    }
+  }
+}
+</script>
---
+++ pages/booths/genre/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+div
+  Hero(title="ジャンル一覧")
+  div.columns.is-multiline
+    nuxt-link(:to="'/booths/genre/food'").button.is-fullwidth.column
+      span 食べる
+      b-icon(icon="chevron-right" size="is-medium")
+    nuxt-link(:to="'/booths/genre/experience'").button.is-fullwidth.column
+      span 体験する
+      b-icon(icon="chevron-right" size="is-medium")
+    nuxt-link(:to="'/booths/genre/game'").button.is-fullwidth.column
+      span 遊ぶ
+      b-icon(icon="chevron-right" size="is-medium")
+    nuxt-link(:to="'/booths/genre/other'").button.is-fullwidth.column
+      span その他
+      b-icon(icon="chevron-right" size="is-medium")
+</template>
+
+<script>
+import Hero from "@/components/Hero.vue";
+
+export default {
+  components:{
+    Hero
+  }
+}
+</script>
